3a06a04ccc9c2edbe875a59889fdf5623962be69
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-03-19 15:11:15 -0700

    Styled react page using styled-components. Stretch Goal: Also styled using Emotion.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const BigH1 = styled.h1 `
+    background-color: dodgerblue;
+    color: black;
+    font-size: 5rem;
+`;
+
+const PodImg = styled.img `
+    height: 70vh;
+    width: 80%;
+    border-radius: 5%;
+    border: 5px solid dodgerblue;
+`;
 
 const CardMaker = props => {
     // console.log(props);
     return (
         <div className="cardDiv">
-            <h1>NASA Photo of the Day!!!</h1>
-            <img className="cardImg" alt="Nasa Photo of the Day" src={props.photo.url} />
+            <BigH1>NASA Photo of the Day!!!</BigH1>
+            <PodImg className="cardImg" alt="Nasa Photo of the Day" src={props.photo.url} />
         </div>
     )
 }
--- src/components/SecondCard.js
+++ src/components/SecondCard.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+// const RoverDiv = styled.div `
+//     width: 40%;
+// `;
+
+// const RoverImg = styled.img `
+//     width: 100%;
+//     border-radius: 5%;
+//     border: 5px solid crimson;
+//     margin-bottom: 5vh;
+// `;
 
 const SecondCardMaker = props => {
     console.log(props);
     return (
-        <div className="cardDiv">
-            <h1>Mars Rover Photos!!!</h1>
-            <img className="cardImg" alt="Curiosity Rover Mast Pictures" src={props.photo} />
+        <div css={css `width: 40%;`} className="cardDiv">
+            <img css={css `width: 100%; border-radius: 5%; border: 5px solid crimson; margin-bottom: 5vh;`} className="cardImg" alt="Curiosity Rover Mast Pictures" src={props.photo} />
         </div>
     )
 }
